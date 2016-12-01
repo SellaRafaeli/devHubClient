f604264f012ac1393c4ea821de67a955d0a8f9ec
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-12-01 12:33:07 +0200

    auto-message.

---
+++ js/lib.js
@@ -0,0 +1,19 @@
+function getHTML(path, selector = 'body') {
+  $.get(path).success(res => $(selector).append(res));
+}
+
+function log(str) { console.log(str) }
+
+function winHash() { 
+  return window.location.href.split("#")[1].split('?')[0];
+}
+
+qs = function winHashQSObj(){
+  var qs = window.location.href.split("#")[1].toString().split('?')[1];
+  return qsToObj(qs);
+}
+
+function qsToObj(str) {
+    if (!str) return {};
+    var pieces = str.split("&"), data = {}, i, parts; for (i = 0; i < pieces.length; i++) {parts = pieces[i].split("="); if (parts.length < 2) {parts.push(""); } data[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]); } return data;
+}
---
+++ js/nav.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  window.location.hash = window.location.hash || '#posts'
+  window.onhashchange = navigateToHash;
+
+  function updatePage(page) {
+    var pageLoader = window[page+'Update'];
+    if (pageLoader) pageLoader();
+  }
+
+  function navigateToHash() {  
+    var sec = winHash();
+    $('body > section').hide();
+    $('#'+sec).show();
+    
+    updatePage(sec)
+  }
+
+  navigateToHash();
+});
